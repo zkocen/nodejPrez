ff724f344b358e1b7147cccffa3f17f640d543ee
Zan Kocen <incubator@wds.co>
2014-05-28 20:30:27 +0100

    Ready for heroku

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ app.get('/game', function(req,res){
   	res.send(games);
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
